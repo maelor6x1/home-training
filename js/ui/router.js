962b5c7bc6706cf553967f18f6e0d50bb7dd413c
maelor6x1 <r5y2vnn98p@privaterelay.appleid.com>
2026-09-23 14:04:12 -0300

    Delete js directory

--- js/ui/router.js
+++
@@ -1,3 +0,0 @@
-import {$,$$} from '../core/utils.js';
-export function showScreen(id){$$('.screen').forEach(s=>s.classList.toggle('hidden',s.dataset.screen!==id));$$('.tab').forEach(t=>t.classList.toggle('active',t.dataset.tab===id));window.scrollTo?.(0,0)}
-export function bindTabs(){ $$('.tab').forEach(t=>t.addEventListener('click',()=>showScreen(t.dataset.tab))) }
